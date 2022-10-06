61c254dc3e3ed203dffcc3d514757786f2d494c2
Shubhang-2111 <90391327+Shubhang-2111@users.noreply.github.com>
2022-10-06 22:31:49 +0530

    Create main.cpp

---
+++ searching/binary_search_recursion/main.cpp
@@ -0,0 +1,30 @@
+// C++ program to implement Binary Search using Recursion
+#include <iostream.h>
+using namespace std;
+ 
+
+int binarySearch(int arr[], int l, int r, int x)
+{
+   // enter your code here
+}
+ 
+
+int main()
+{   
+    int size;
+    cin>>size; //size of array
+    int arr[size];
+    
+    for(int i=0;i<size;i++){
+       cin>>arr[i]; // elemnents in the array
+    }
+   
+   int element; // element to be searched
+    cin>>element;
+ 
+    int result = binarySearch(arr, 0, size - 1, element);
+    (result == -1)
+        ? cout << "Element is not present in array"
+        : cout << "Element is present at index " << result;
+    return 0;
+}
