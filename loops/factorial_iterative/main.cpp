a183df967d4dd38ed3937b1eda8ff70217195aa6
Shubhang-2111 <90391327+Shubhang-2111@users.noreply.github.com>
2022-10-06 15:21:48 +0530

    Create main.cpp

---
+++ loops/factorial_iterative/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ 
+void factorial(int n){
+   //Enter your code here
+   // print the answer in the fucntion itself
+}
+int main()
+{   
+    int n;
+    cin>>n;
+    factorial(n);
+    return 0;
+}
