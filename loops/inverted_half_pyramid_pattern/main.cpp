9ae9b6ad5e20991e7737d0967f932c3a86acda72
Shubhang-2111 <90391327+Shubhang-2111@users.noreply.github.com>
2022-10-07 01:26:27 +0530

    Create main.cpp

---
+++ loops/inverted_half_pyramid_pattern/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ 
+void inverted_half_pyramid_pattern(int n){
+   //Enter your code here
+}
+int main()
+{   
+    int n;
+    cin>>n;
+    inverted_half_pyramid_pattern(n);
+    return 0;
+}
