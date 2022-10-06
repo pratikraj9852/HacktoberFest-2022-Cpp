3bd7c1ddd0baeddacc47ce10c118bac27233081b
Shubhang-2111 <90391327+Shubhang-2111@users.noreply.github.com>
2022-10-06 22:29:37 +0530

    Update main.cpp

--- searching/binary_search/main.cpp
+++ searching/binary_search/main.cpp
@@ -1,21 +1,27 @@
-// C++ program to implement recursive Binary Search
-#include <bits/stdc++.h>
+// C++ program to implement Binary Search
+#include <iostream.h>
 using namespace std;
  
-// A recursive binary search function. It returns
-// location of x in given array arr[l..r] is present,
-// otherwise -1
+
 int binarySearch(int arr[], int l, int r, int x)
 {
    // enter your code here
 }
  
-int main(void)
-{
-    int arr[] = { 2, 3, 4, 10, 40 };
-    int x = 10;
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int result = binarySearch(arr, 0, n - 1, x);
+int main()
+{   
+    int size;
+    cin>>size; //size of array
+    int arr[size];
+    
+    for(int i=0;i<size;i++){
+       cin>>arr[i]; // elemnents in the array
+    }
+   
+   int element; // element to be searched
+    cin>>element;
+ 
+    int result = binarySearch(arr, 0, size - 1, element);
     (result == -1)
         ? cout << "Element is not present in array"
         : cout << "Element is present at index " << result;
