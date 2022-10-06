783c52a46df2bd059bcc233af3113ca937566824
Shubhang-2111 <90391327+Shubhang-2111@users.noreply.github.com>
2022-10-07 01:24:58 +0530

    Create main.cpp

---
+++ loops/hourglass_pattern/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ 
+void diamond_pattern(int n){
+   //Enter your code here
+}
+//Driver Code
+int main()
+{   
+    int n;
+    cin>>n;
+    diamond_pattern(n);
+    return 0;
+}
