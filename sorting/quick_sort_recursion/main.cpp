b897deec0869e2bcc178548b2100391b052884f6
Shubhang-2111 <90391327+Shubhang-2111@users.noreply.github.com>
2022-10-07 01:04:00 +0530

    Create main.cpp

---
+++ sorting/quick_sort_recursion/main.cpp
@@ -0,0 +1,33 @@
+// C++ program to implement Quick Sort using recursion
+#include <iostream>
+using namespace std;
+ 
+
+void quickSort_recursion(int arr[],int begin,int end)
+{  
+         // enter your code here
+}
+
+
+void printArray(int arr[], int n)  // function to print elements of array
+{
+    for (int i=0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+int main()
+{   
+    int size;
+    cin>>size; //size of array
+    int arr[size];
+    
+    for(int i=0;i<size;i++){
+       cin>>arr[i]; // elemnents in the array
+    }
+   
+   quickSort_recursion(arr,0,size-1);
+   
+   printArray(arr, size);
+   return 0;
+}
