237fba000545ef51e922378bbc5198f21fc38c2d
Shubhang-2111 <90391327+Shubhang-2111@users.noreply.github.com>
2022-10-06 15:18:06 +0530

    Create main.cpp

---
+++ loops/fibonacci_iterative/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ 
+void fibonacci(int n){
+   //Enter your code here
+   // print the answer in the fucntion itself
+}
+int main()
+{   
+    int n;
+    cin>>n;
+    fibonacci(n);
+    return 0;
+}
