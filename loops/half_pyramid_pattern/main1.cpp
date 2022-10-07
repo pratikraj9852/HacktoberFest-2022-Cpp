3023947ba54beed6635bfd7a6aa284837444926e
Shubhang-2111 <90391327+Shubhang-2111@users.noreply.github.com>
2022-10-07 22:18:46 +0530

    Create main1.cpp

---
+++ loops/half_pyramid_pattern/main1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ 
+void half_pyramid_pattern(int n){
+   //Enter your code here
+}
+int main()
+{   
+    int n;
+    cin>>n;
+    half_pyramid_pattern(n);
+    return 0;
+}
