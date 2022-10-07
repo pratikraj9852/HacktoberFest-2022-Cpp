aea43ee56d3330d603044d02ce8957b0be1f568c
Shubhang-2111 <90391327+Shubhang-2111@users.noreply.github.com>
2022-10-07 22:13:50 +0530

    Create main1.cpp

---
+++ basic/swap_numbers_without_using_third_variable/main1.cpp
@@ -0,0 +1,17 @@
+//program to swap two numbers
+#include<iostream>
+using namespace std;
+ 
+void swap(int &a,int &b){
+   //Enter your code here
+}
+
+//Driver Code
+int main()
+{   
+    int a,b;
+    cin>>a>>b;
+    swap(a,b);
+    cout<<a<<" "<<b;
+    return 0;
+}
