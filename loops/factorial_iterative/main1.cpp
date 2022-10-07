6b05838e244fb838e6f724b36d64aa1ec5bb1a62
Shubhang-2111 <90391327+Shubhang-2111@users.noreply.github.com>
2022-10-07 22:15:33 +0530

    Create main1.cpp

---
+++ loops/factorial_iterative/main1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ 
+double factorial(int n){
+   //Enter your code here
+}
+
+//Driver Code
+int main()
+{   
+    int n;
+    cin>>n;
+    cout<<factorial(n);
+    return 0;
+}
