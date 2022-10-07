4539da6fd65a7e51ee15a7cf9080f94a8cac8835
Shubhang-2111 <90391327+Shubhang-2111@users.noreply.github.com>
2022-10-07 22:19:18 +0530

    Create main1.cpp

---
+++ loops/hourglass_pattern/main1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ 
+void hourglass_pattern(int n){
+   //Enter your code here
+}
+//Driver Code
+int main()
+{   
+    int n;
+    cin>>n;
+    hourglass_pattern(n);
+    return 0;
+}
