b285f11a179a11b702055c23d7a13a9009261d58
Shubhang-2111 <90391327+Shubhang-2111@users.noreply.github.com>
2022-10-07 15:11:02 +0530

    Create main.cpp

---
+++ basic/largest_of_three_numbers/main.cpp
@@ -0,0 +1,18 @@
+//program to find greatest of three numbers
+#include<iostream>
+using namespace std;
+ 
+int largest(int n){
+   //Enter your code here
+}
+
+//Driver Code
+int main()
+{   
+    int a,b,c;
+    cin>>a>>b>>c;
+    
+  
+    
+    return 0;
+}
