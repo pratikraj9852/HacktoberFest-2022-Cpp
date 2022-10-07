05f293c907c7f4b15d0708a7933766a6fc248750
Shubhang-2111 <90391327+Shubhang-2111@users.noreply.github.com>
2022-10-07 22:21:00 +0530

    Create main1.cpp

---
+++ loops/right_angle_triangle_pattern/main1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ 
+void right_angle_triangle_pattern(int n){
+   //Enter your code here
+}
+
+//Driver Code
+int main()
+{   
+    int n;
+    cin>>n;
+    right_angle_triangle_pattern(n);
+    return 0;
+}
