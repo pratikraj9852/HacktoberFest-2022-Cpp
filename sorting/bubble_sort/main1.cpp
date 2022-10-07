796a934f5128e5b912af19767cc4fffb9d5d7691
Shubhang-2111 <90391327+Shubhang-2111@users.noreply.github.com>
2022-10-07 23:45:41 +0530

    Create main1.cpp

---
+++ sorting/bubble_sort/main1.cpp
@@ -0,0 +1,34 @@
+// C++ program to implement Bubble Sort
+#include <iostream>
+using namespace std;
+ 
+
+void bubbleSort(int arr[],int size)
+{  
+         // enter your code here
+}
+
+
+void printArray(int arr[], int n)  // function to print elements of array
+{
+    for (int i=0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+//Driver Code
+int main()
+{   
+    int size;
+    cin>>size; //size of array
+    int arr[size];
+    
+    for(int i=0;i<size;i++){
+       cin>>arr[i]; // elemnents in the array
+    }
+   
+   bubbleSort(arr,size);
+   
+   printArray(arr, size);
+   return 0;
+}
