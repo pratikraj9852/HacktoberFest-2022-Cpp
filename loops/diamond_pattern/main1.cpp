ffc3e15d5ec2031f1b41a6b5f5981afc581bb363
Shubhang-2111 <90391327+Shubhang-2111@users.noreply.github.com>
2022-10-07 22:15:06 +0530

    Create main1.cpp

---
+++ loops/diamond_pattern/main1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ 
+void diamond_pattern(int n){
+   //Enter your code here
+}
+int main()
+{   
+    int n;
+    cin>>n;
+    diamond_pattern(n);
+    return 0;
+}
