25ffeba3d5960198b39ea9aa9ba410b9c9d270bb
Shubhang-2111 <90391327+Shubhang-2111@users.noreply.github.com>
2022-10-07 22:16:56 +0530

    Create main1.cpp

---
+++ loops/half_diamond_pattern/main1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ 
+void half_diamond_pattern(int n){
+   //Enter your code here
+}
+int main()
+{   
+    int n;
+    cin>>n;
+    half_diamond_pattern(n);
+    return 0;
+}
