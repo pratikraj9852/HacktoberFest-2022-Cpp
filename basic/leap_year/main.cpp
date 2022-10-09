a74a0a49c4d84a0b3af463f4f1962dfeec229460
pratikraj9852 <114500899+pratikraj9852@users.noreply.github.com>
2022-10-09 22:12:55 +0530

    Update main.cpp

--- basic/leap_year/main.cpp
+++ basic/leap_year/main.cpp
@@ -1,14 +1,25 @@
 //program to find whether given year is leap year or not
 #include<iostream>
 using namespace std;
- 
+
 bool leap_year(int n){
    //Enter your code here
+ if (n % 400 == 0)
+        return true;
+
+
+    if (n % 100 == 0)
+        return false;
+
+
+    if (n % 4 == 0)
+        return true;
+    return false;
 }
 
 //Driver Code
 int main()
-{   
+{
     int n;
     cin>>n;
     if(leap_year(n))
